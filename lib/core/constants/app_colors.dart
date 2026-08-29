aa3def9d4ccc784995b15bca44d26779ea5ab110
Mohammad <mnikmard200@gmail.com>
2026-08-29 20:38:01 +0330

    chore: add app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final class AppColors {
+  static const primary900 = Color(0xff1A1A1A);
+  static const primary800 = Color(0xff333333);
+  static const primary700 = Color(0xff4D4D4D);
+  static const primary600 = Color(0xff666666);
+  static const primary500 = Color(0xff808080);
+  static const primary400 = Color(0xff999999);
+  static const Color primary300 = Color(0xffB3B3B3);
+  static const Color primary200 = Color(0xffCCCCCC);
+  static const Color primary100 = Color(0xffE6E6E6);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color success = Color(0xff0C9409);
+  static const Color error = Color(0xffED1010);
+}
