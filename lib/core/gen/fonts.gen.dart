d215537c3b5e9b0c5249e9c2efe99890f8bbb42f
Mohammad <mnikmard200@gmail.com>
2026-08-29 20:54:07 +0330

    chore: add fonts in app

---
+++ lib/core/gen/fonts.gen.dart
@@ -0,0 +1,20 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+abstract final class FontFamily {
+  /// Font family: NSM
+  static const String nsm = 'NSM';
+
+  /// Font family: NSR
+  static const String nsr = 'NSR';
+
+  /// Font family: NSSB
+  static const String nssb = 'NSSB';
+}
