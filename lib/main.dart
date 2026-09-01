f48d58ba5722a82e3f9b0999c0e75218778678d6
Mohammad <mnikmard2004@gmail.com>
2026-09-01 13:31:32 +0330

    chore: cleared up main

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 26),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('HELLO'),
-                const SizedBox(height: 40),
-                CtaFactory.selectCTA(
-                  type: CtaType.facebookLogin,
-                  data: CtaData(text: 'Sign Up with Facebook', onTapped: () {}),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello'))),
     );
   }
 }
